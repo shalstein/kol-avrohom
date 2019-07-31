3dc09e2063e40505f686f3201b96537655342992
Sholom <boruchsteinmetz@outlook.com>
2019-07-30 22:21:18 -0400

    change home page title to hebrew and app name to hebrew

--- src/app/tractates/tractates.page.ts
+++ src/app/tractates/tractates.page.ts
@@ -6,7 +6,7 @@ import { ActivatedRoute} from '@angular/router';
   styleUrls: ['./tractates.page.scss'],
 })
 export class TractatesPage implements OnInit {
-  currentSeder = 'taharos';
+  currentSeder: string;
   // tslint:disable-next-line:max-line-length
   mesechtos = {זרעים: ['ברכות'], מועד: ['שבת', 'עירובין', 'פסחים' , 'ראש השנה', 'יומא', 'סוכה', 'ביצה', 'תענית', 'מגילה', 'מועד קטן',   'חגיגה'], נשים: ['יבמות', 'כתובות', 'נדרים', 'נזיר', 'סוטה', 'גיטין', 'קידושין'  ], נזקין: ['בבא קמא', 'בבא מציעא', 'בבא בתרא', 'סנהדרין', 'מכות', 'שבועות', 'עבודה זרה', 'הוריות' ], קדשים: ['זבחים', 'מנחות', 'חולין', 'בכורות', 'ערכין', 'תמורה', 'כריתות', 'מעילה', ], טהרות: ['נדה']};
 
