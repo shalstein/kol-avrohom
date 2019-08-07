b3ea073df1023ad0d19e9766bc7edac267cd4ff2
Sholom <boruchsteinmetz@outlook.com>
2019-08-07 10:48:19 -0400

    refactor handling page number param to use array

--- src/app/tractate/tractate.page.ts
+++ src/app/tractate/tractate.page.ts
@@ -16,8 +16,36 @@ export class TractatePage implements OnInit {
   currentPage = '02';
   audioURL: string;
   tractatesMetadata = {
-    // tslint:disable-next-line:max-line-length
-    ברכות: {name: 'Brochos', lastPage: 64}, שבת: {name: 'Shabbos', lastPage: 157}, עירובין: {name: 'Eruvin', lastPage: 104}, פסחים: {name: 'Pesachim', lastPage: 120}, 'ראש השנה': {name: 'RoshHashana', lastPage: 35} , יומא : {name: 'Yuma', lastPage: 88}, סוכה: {name: 'Succah', lastPage: 56}, ביצה: {name: 'Beitzah', lastPage: 40}, תענית: {name: 'Taanis', lastPage: 31}, מגילה: {name: 'Megillah', lastPage: 32}, 'מועד קטן': {name: 'MoedKatan', lastPage: 29}, חגיגה: {name: 'Chagigah', lastPage: 27}, יבמות: {name: 'Yevamos', lastPage: 122}, כתובות: {name: 'Kesubos', lastPage: 112}, נדרים: {name: 'Nedarim', lastPage: 91}, נזיר: {name: 'Nozir', lastPage: 66}, סוטה: {name: 'Sotah', lastPage: 49} , גיטין: {name: 'Gitin', lastPage: 90}, קידושין: {name: 'Kiddushin', lastPage: 82}, 'בבא קמא': {name: 'BabaKama', lastPage: 119}, 'בבא מציעא': {name: 'BabaMetzia', lastPage: 119}, 'בבא בתרא': {name: 'BabaBasra', lastPage: 176}, סנהדרין: {name: 'Sanhedrin', lastPage: 113}, מכות: {name: 'Makot', lastPage: 24}, שבועות: {name: 'Shevuot', lastPage: 49}, 'עבודה זרה': {name: 'AvodaZarah', lastPage: 76}, הוריות: {name: 'Horayos', lastPage: 14}, זבחים: {name: 'Zevachim', lastPage: 120}, מנחות: {name: 'Menachos', lastPage: 110}, חולין: {name: 'Chulin', lastPage: 64}, בכורות: {name: 'Bechoros', lastPage: 61}, ערכין: {name: 'Erchin', lastPage: 34}, תמורה: {name: 'Temura', lastPage: 34}, כריתות: {name: 'Kerisus', lastPage: 28}, מעילה: {name: 'Meilah', lastPage: 22}, נדה: {name: 'Niddah', lastPage: 73}
+    ברכות: {name: 'Brochos', lastPage: 64}, שבת: {name: 'Shabbos',
+     lastPage: 157}, עירובין: {name: 'Eruvin', lastPage: 104}, פסחים: {name: 'Pesachim', lastPage: 120}, 'ראש השנה': {name: 'RoshHashana',
+      lastPage: 35} , יומא : {name: 'Yuma', lastPage: 88},
+     סוכה: {name: 'Succah', lastPage: 56}, ביצה: {name: 'Beitzah',
+      lastPage: 40}, תענית: {name: 'Taanis', lastPage: 31},
+       מגילה: {name: 'Megillah', lastPage: 32},
+    'מועד קטן': {name: 'MoedKatan', lastPage: 29},
+     חגיגה: {name: 'Chagigah', lastPage: 27},
+     יבמות: {name: 'Yevamos', lastPage: 122},
+     כתובות: {name: 'Kesubos', lastPage: 112},
+     נדרים: {name: 'Nedarim', lastPage: 91},
+     נזיר: {name: 'Nozir', lastPage: 66},
+     סוטה: {name: 'Sotah', lastPage: 49} ,
+     גיטין: {name: 'Gitin', lastPage: 90},
+     קידושין: {name: 'Kiddushin', lastPage: 82},
+     'בבא קמא': {name: 'BabaKama', lastPage: 119},
+     'בבא מציעא': {name: 'BabaMetzia', lastPage: 119},
+     'בבא בתרא': {name: 'BabaBasra', lastPage: 176},
+      סנהדרין: {name: 'Sanhedrin', lastPage: 113},
+       מכות: {name: 'Makot', lastPage: 24},
+        שבועות: {name: 'Shevuot', lastPage: 49},
+       'עבודה זרה': {name: 'AvodaZarah', lastPage: 76},
+        הוריות: {name: 'Horayos', lastPage: 14},
+        זבחים: {name: 'Zevachim', lastPage: 120},
+        מנחות: {name: 'Menachos', lastPage: 110},
+        חולין: {name: 'Chulin', lastPage: 64},
+        בכורות: {name: 'Bechoros', lastPage: 61},
+       ערכין: {name: 'Erchin', lastPage: 34},
+       תמורה: {name: 'Temura', lastPage: 34},
+       כריתות: {name: 'Kerisus', lastPage: 28}, מעילה: {name: 'Meilah', lastPage: 22}, נדה: {name: 'Niddah', lastPage: 73}
   };
 
   pageValues = ['02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12',
@@ -48,8 +76,7 @@ export class TractatePage implements OnInit {
     this.audioURL = `http://download.kolavrohom.com/${this.tractateEnglishName}/${this.currentPage}.mp3`;
     this.tractatePages = this.generateTractatePages(this.tractatesMetadata[this.tractate].lastPage);
     if (tractatePageNumber) {
-      const pageNumberPrefix = parseInt(tractatePageNumber, 10) <= 9 ? '0' : '';
-      this.currentPage =  pageNumberPrefix + tractatePageNumber;
+      this.currentPage =  this.pageValues[(+ tractatePageNumber) - 2];
       this.handleSelectDafChange(this.audioPlayer.nativeElement);
     }
  }
